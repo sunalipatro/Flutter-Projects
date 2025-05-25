22ca21ec3ae33f867aeea326b6da81f3c06246cc
sunalipatro <sunalipatro6@gmail.com>
2025-05-25 13:58:29 +0530

    to do list app

---
+++ to_do_list_app_firebase/android/app/src/main/kotlin/com/example/to_do_list_app_firebase/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.to_do_list_app_firebase
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ to_do_list_app_firebase/lib/main.dart
@@ -0,0 +1,30 @@
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:firebase_core/firebase_core.dart';
+import 'package:flutter/material.dart';
+
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp();
+  runApp(MyApp());
+}
+
+class MyApp extends StatefulWidget {
+  MyApp({Key? key}) : super(key: key);
+
+  @override
+  _MyAppState createState() => _MyAppState();
+}
+
+class _MyAppState extends State<MyApp> {
+  firebase_auth.FirebaseAuth firebaseAuth = firebase_auth.FirebaseAuth.instance;
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Scaffold(
+          appBar: AppBar(
+        title: const Text("To-do-list"),
+      )),
+    );
+  }
+}
